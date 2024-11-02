2780d84ef700961f579bd3cc1e1d2f8875565e01
rebekacep <rebeka.cep@student.um.si>
2024-11-02 20:38:05 +0100

    Changes in controllers, routes, local storage

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,5 @@
 const bcrypt = require("bcryptjs");
 const User = require("../models/userModel");
-const Request = require("../models/requestModel");
 const jwt = require("jsonwebtoken");
 
 exports.registerUser = async (req, res) => {
@@ -128,48 +127,5 @@ exports.toggleUserTypeById = async (req, res) => {
   } catch (error) {
     console.error("Error in toggleUserTypeById:", error);
     res.status(500).json({ message: "Server error" });
-  }
-};
-
-exports.createRequest = (req, res) => {
-  try {
-    const { dopusti } = req.body;
-    const userId = req.userId;
-
-    const newRequest = {
-      uporabnik_id: userId,
-      datum_zahteve: new Date(),
-      stanje: "in progress",
-    };
-
-    Request.create(newRequest, (error, result) => {
-      if (error) {
-        console.error("Error creating request:", error);
-        return res.status(500).json({ message: "Failed to create request" });
-      }
-
-      const requestId = result.insertId;
-
-      dopusti.forEach((dopust) => {
-        const dopustData = {
-          zacetek: dopust.startDate,
-          konec: dopust.endDate,
-          razlog: dopust.razlog,
-          tip_dopusta_id: dopust.tip,
-          zahteva_id: requestId,
-        };
-
-        Request.createLeave(dopustData, (leaveError) => {
-          if (leaveError) {
-            console.error("Error creating leave:", leaveError);
-          }
-        });
-      });
-
-      res.status(201).json({ message: "Request created successfully" });
-    });
-  } catch (error) {
-    console.error("Unexpected error:", error);
-    res.status(500).json({ message: "Unexpected error occurred" });
   }
 };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
-const verifyToken = require('../middleware/verifyToken');
+const userController = require("../controllers/userController");
+const requestController = require("../controllers/requestController");
+const verifyToken = require("../middleware/verifyToken");
 
-router.post('/', userController.registerUser);
-router.get('/', userController.getAllUsers);
-router.post('/login', userController.loginUser);
-router.get('/loggedIn', verifyToken, userController.getLoggedInUser);
-router.put('/toggle/:id', userController.toggleUserTypeById);
+router.post("/", userController.registerUser);
+router.get("/", userController.getAllUsers);
+router.post("/login", userController.loginUser);
+router.get("/loggedIn", verifyToken, userController.getLoggedInUser);
+router.put("/toggle/:id", userController.toggleUserTypeById);
+router.post("/request", verifyToken, requestController.createRequest);
 module.exports = router;
